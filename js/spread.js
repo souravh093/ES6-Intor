f8c59da3f4cc25151aeae7474ca633b2c90810a9
Sourav halder <souravh093@gmail.com>
2023-02-20 17:16:32 +0600

    spread js learning

---
+++ js/spread.js
@@ -0,0 +1,17 @@
+const max = Math.max(13, 82, 99, 123);
+// console.log(max);
+
+const numbers = [12, 32, 923, 1234, 1235, 12];
+
+const largest = Math.max(numbers);
+// console.log(Math.max(...numbers));
+// console.log(numbers);
+// console.log(...numbers);
+// console.log(largest);
+
+const numbers2 = [...numbers];
+numbers.push(532);
+numbers2.push(432); 
+console.log(numbers);
+console.log(numbers2);
+
